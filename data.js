60294a88219f8deb70623138519106f2af3ee0d1
anhthunguyenhaitrieu-sys <anhthunguyen.haitrieu@gmail.com>
2026-05-29 22:15:08 +0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,58 @@
+const evaluationData = {
+    "gdoc_hcm": [
+        { name: "Dương Hiển Đôn", van_hoa: 1, van_hoa_chuan: 3, tuyen_dung: 1, tuyen_dung_chuan: 3, kinh_doanh: 1, kinh_doanh_chuan: 3 },
+        { name: "Chiêu Quốc Tuấn", van_hoa: 2, van_hoa_chuan: 3, tuyen_dung: 1, tuyen_dung_chuan: 3, kinh_doanh: 1, kinh_doanh_chuan: 3 },
+        { name: "Lê Minh Tài", van_hoa: 2, van_hoa_chuan: 3, tuyen_dung: 1.3, tuyen_dung_chuan: 3, kinh_doanh: 1.3, kinh_doanh_chuan: 3 },
+        { name: "Trần Quang Vinh", van_hoa: 3, van_hoa_chuan: 3, tuyen_dung: 3, tuyen_dung_chuan: 3, kinh_doanh: 3, kinh_doanh_chuan: 3 },
+        { name: "Lê Minh Hoàng", van_hoa: 2.5, van_hoa_chuan: 3, tuyen_dung: 2.5, tuyen_dung_chuan: 3, kinh_doanh: 2.5, kinh_doanh_chuan: 3 },
+        { name: "Lê Quốc Cường", van_hoa: 3, van_hoa_chuan: 3, tuyen_dung: 3, tuyen_dung_chuan: 3, kinh_doanh: 3, kinh_doanh_chuan: 3 },
+        { name: "Nguyễn Thị Thu Lan", van_hoa: 2.1, van_hoa_chuan: 3, tuyen_dung: 2.1, tuyen_dung_chuan: 3, kinh_doanh: 2.1, kinh_doanh_chuan: 3 },
+        { name: "Đặng Ngọc Châu", van_hoa: 2.3, van_hoa_chuan: 3, tuyen_dung: 2.3, tuyen_dung_chuan: 3, kinh_doanh: 2.3, kinh_doanh_chuan: 3 },
+        { name: "Nguyễn Văn Hậu", van_hoa: 1.1, van_hoa_chuan: 3, tuyen_dung: 0.5, tuyen_dung_chuan: 3, kinh_doanh: 0.5, kinh_doanh_chuan: 3 },
+        { name: "Võ Trần Tấn Nam", van_hoa: 2.2, van_hoa_chuan: 3, tuyen_dung: 2.2, tuyen_dung_chuan: 3, kinh_doanh: 2.2, kinh_doanh_chuan: 3 },
+        { name: "Lê Trung Kiên", van_hoa: 1.2, van_hoa_chuan: 3, tuyen_dung: 1.2, tuyen_dung_chuan: 3, kinh_doanh: 1.2, kinh_doanh_chuan: 3 },
+        { name: "Phạm Đinh Sang", van_hoa: 1.6, van_hoa_chuan: 3, tuyen_dung: 3, tuyen_dung_chuan: 3, kinh_doanh: 1.5, kinh_doanh_chuan: 3 }
+    ],
+    "gdoc_dnb": [
+        { name: "Dương Hiển A", van_hoa: 2, van_hoa_chuan: 3, tuyen_dung: 1, tuyen_dung_chuan: 3, kinh_doanh: 2, kinh_doanh_chuan: 3 },
+        { name: "Chiêu Quốc B", van_hoa: 1.1, van_hoa_chuan: 3, tuyen_dung: 2, tuyen_dung_chuan: 3, kinh_doanh: 1, kinh_doanh_chuan: 3 },
+        { name: "Lê Minh C", van_hoa: 2, van_hoa_chuan: 3, tuyen_dung: 1.3, tuyen_dung_chuan: 3, kinh_doanh: 1, kinh_doanh_chuan: 3 },
+        { name: "Trần Quang D", van_hoa: 3, van_hoa_chuan: 3, tuyen_dung: 3, tuyen_dung_chuan: 3, kinh_doanh: 3, kinh_doanh_chuan: 3 },
+        { name: "Lê Minh E", van_hoa: 3, van_hoa_chuan: 3, tuyen_dung: 1, tuyen_dung_chuan: 3, kinh_doanh: 2.5, kinh_doanh_chuan: 3 },
+        { name: "Lê Quốc F", van_hoa: 3, van_hoa_chuan: 3, tuyen_dung: 3, tuyen_dung_chuan: 3, kinh_doanh: 3, kinh_doanh_chuan: 3 },
+        { name: "Nguyễn Thị Thu G", van_hoa: 0.4, van_hoa_chuan: 3, tuyen_dung: 2.1, tuyen_dung_chuan: 3, kinh_doanh: 2.1, kinh_doanh_chuan: 3 },
+        { name: "Đặng Ngọc H", van_hoa: 2.3, van_hoa_chuan: 3, tuyen_dung: 2.3, tuyen_dung_chuan: 3, kinh_doanh: 3, kinh_doanh_chuan: 3 },
+        { name: "Nguyễn Văn J", van_hoa: 2.2, van_hoa_chuan: 3, tuyen_dung: 0.5, tuyen_dung_chuan: 3, kinh_doanh: 0.5, kinh_doanh_chuan: 3 },
+        { name: "Võ Trần Tấn K", van_hoa: 2.2, van_hoa_chuan: 3, tuyen_dung: 3, tuyen_dung_chuan: 3, kinh_doanh: 0.6, kinh_doanh_chuan: 3 },
+        { name: "Lê Trung L", van_hoa: 1.2, van_hoa_chuan: 3, tuyen_dung: 1.2, tuyen_dung_chuan: 3, kinh_doanh: 1.2, kinh_doanh_chuan: 3 },
+        { name: "Phạm Đinh M", van_hoa: 1, van_hoa_chuan: 3, tuyen_dung: 3, tuyen_dung_chuan: 3, kinh_doanh: 3, kinh_doanh_chuan: 3 }
+    ],
+    "tpkd_hcm": [
+        { name: "Dương Hi", van_hoa: 2, van_hoa_chuan: 3, tuyen_dung: 1, tuyen_dung_chuan: 3, kinh_doanh: 2, kinh_doanh_chuan: 3 },
+        { name: "Chiêu Q", van_hoa: 1.1, van_hoa_chuan: 3, tuyen_dung: 0.5, tuyen_dung_chuan: 3, kinh_doanh: 1, kinh_doanh_chuan: 3 },
+        { name: "Lê Minh", van_hoa: 2, van_hoa_chuan: 3, tuyen_dung: 1.3, tuyen_dung_chuan: 3, kinh_doanh: 1, kinh_doanh_chuan: 3 },
+        { name: "Trần Q", van_hoa: 3, van_hoa_chuan: 3, tuyen_dung: 3, tuyen_dung_chuan: 3, kinh_doanh: 2, kinh_doanh_chuan: 3 },
+        { name: "Lê M", van_hoa: 1, van_hoa_chuan: 3, tuyen_dung: 1.5, tuyen_dung_chuan: 3, kinh_doanh: 2.5, kinh_doanh_chuan: 3 },
+        { name: "Lê Q", van_hoa: 1, van_hoa_chuan: 3, tuyen_dung: 3, tuyen_dung_chuan: 3, kinh_doanh: 3, kinh_doanh_chuan: 3 },
+        { name: "Nguyễn T", van_hoa: 0.4, van_hoa_chuan: 3, tuyen_dung: 1.1, tuyen_dung_chuan: 3, kinh_doanh: 2.1, kinh_doanh_chuan: 3 },
+        { name: "Đặng N", van_hoa: 1, van_hoa_chuan: 3, tuyen_dung: 0.3, tuyen_dung_chuan: 3, kinh_doanh: 3, kinh_doanh_chuan: 3 },
+        { name: "Nguyễn V", van_hoa: 2.2, van_hoa_chuan: 3, tuyen_dung: 0.5, tuyen_dung_chuan: 3, kinh_doanh: 2, kinh_doanh_chuan: 3 },
+        { name: "Võ Trần T", van_hoa: 1, van_hoa_chuan: 3, tuyen_dung: 1, tuyen_dung_chuan: 3, kinh_doanh: 0.6, kinh_doanh_chuan: 3 },
+        { name: "Lê T", van_hoa: 1.2, van_hoa_chuan: 3, tuyen_dung: 1.2, tuyen_dung_chuan: 3, kinh_doanh: 1.2, kinh_doanh_chuan: 3 },
+        { name: "Phạm Đ", van_hoa: 1, van_hoa_chuan: 3, tuyen_dung: 1.1, tuyen_dung_chuan: 3, kinh_doanh: 2, kinh_doanh_chuan: 3 }
+    ],
+    "tpkd_dnb": [
+        { name: "Dương Di", van_hoa: 2, van_hoa_chuan: 3, tuyen_dung: 1, tuyen_dung_chuan: 3, kinh_doanh: 2, kinh_doanh_chuan: 3 },
+        { name: "Chiêu Quốc Thái", van_hoa: 1.1, van_hoa_chuan: 3, tuyen_dung: 0.5, tuyen_dung_chuan: 3, kinh_doanh: 2, kinh_doanh_chuan: 3 },
+        { name: "Lê Mộng Nga", van_hoa: 0.5, van_hoa_chuan: 3, tuyen_dung: 1.3, tuyen_dung_chuan: 3, kinh_doanh: 1, kinh_doanh_chuan: 3 },
+        { name: "Trần Thị Tâm", van_hoa: 3, van_hoa_chuan: 3, tuyen_dung: 3, tuyen_dung_chuan: 3, kinh_doanh: 2, kinh_doanh_chuan: 3 },
+        { name: "Lê Lan", van_hoa: 1, van_hoa_chuan: 3, tuyen_dung: 1, tuyen_dung_chuan: 3, kinh_doanh: 2.5, kinh_doanh_chuan: 3 },
+        { name: "Lê Lộc", van_hoa: 0.5, van_hoa_chuan: 3, tuyen_dung: 3, tuyen_dung_chuan: 3, kinh_doanh: 2, kinh_doanh_chuan: 3 },
+        { name: "Nguyễn Phương", van_hoa: 0.4, van_hoa_chuan: 3, tuyen_dung: 1.1, tuyen_dung_chuan: 3, kinh_doanh: 2.1, kinh_doanh_chuan: 3 },
+        { name: "Đặng Nga Ngọt", van_hoa: 1, van_hoa_chuan: 3, tuyen_dung: 0.3, tuyen_dung_chuan: 3, kinh_doanh: 3, kinh_doanh_chuan: 3 },
+        { name: "Nguyễn Văn Nam", van_hoa: 2.2, van_hoa_chuan: 3, tuyen_dung: 0.5, tuyen_dung_chuan: 3, kinh_doanh: 2, kinh_doanh_chuan: 3 },
+        { name: "Võ Trần Thy", van_hoa: 1, van_hoa_chuan: 3, tuyen_dung: 1, tuyen_dung_chuan: 3, kinh_doanh: 0.6, kinh_doanh_chuan: 3 },
+        { name: "Lê Tuấn", van_hoa: 1.2, van_hoa_chuan: 3, tuyen_dung: 1, tuyen_dung_chuan: 3, kinh_doanh: 1.2, kinh_doanh_chuan: 3 },
+        { name: "Phạm Đức Tân", van_hoa: 1, van_hoa_chuan: 3, tuyen_dung: 1.1, tuyen_dung_chuan: 3, kinh_doanh: 2, kinh_doanh_chuan: 3 }
+    ]
+};
